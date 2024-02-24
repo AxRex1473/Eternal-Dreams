448a854166556cb890f98019bd73936501f3261a
Axel Hernández <89049642+AxRex1473@users.noreply.github.com>
2024-02-23 19:37:19 -0600

    Animaciones
    Animaciones sencillas

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,25 +4,37 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private Animator animator;
     [SerializeField] private float speedMovement;
-    [SerializeField] private Vector2 direction;
-
-    private Rigidbody2D rb2d;
+    [SerializeField] private Vector2 direction;  
+    [SerializeField] private Rigidbody2D rb2d;
+    [SerializeField] SpriteRenderer spriteRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+        animator.SetFloat("Speed", direction.magnitude);
+
+        if(direction.x < 0) 
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if(direction.x > 0) 
+        {
+            spriteRenderer.flipX = false;
+        }
     }
 
     private void FixedUpdate()
     {
-        rb2d.MovePosition(rb2d.position + direction * speedMovement *  Time.fixedDeltaTime);
+        rb2d.MovePosition(rb2d.position + direction * speedMovement * Time.fixedDeltaTime);
     }
 }
